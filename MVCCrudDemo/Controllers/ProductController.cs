cb31cf2d0cbf5c07c44c410583243ce1abdda4d2
Acer <t.a.tomov@gmail.com>
2021-09-06 01:06:24 +0300

    Add project files.

---
+++ MVCCrudDemo/Controllers/ProductController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCCrudDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCrudDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductDAL product = new ProductDAL();
+        public IActionResult Index()
+        {
+            List<ProductInfo> productsList = new List<ProductInfo>();
+            productsList = product.GetAllProducts().ToList();
+            return View(productsList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind] ProductInfo obj)
+        {
+            if (ModelState.IsValid)
+            {
+                product.AddProduct(obj);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null) {
+                return NotFound();            
+            }
+
+            ProductInfo prod = product.GetProductByID(id);
+
+            if (prod == null)
+            {
+                return NotFound();
+            }
+            
+            return View(prod);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int? id, [Bind] ProductInfo objProd)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid) {
+                product.UpdateProduct(objProd);
+                return RedirectToAction("Index");   
+            }
+
+            return View(product);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProductInfo prod = product.GetProductByID(id);
+
+            if (prod == null)
+            {
+                return NotFound();
+            }
+
+            return View(prod);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int? id, [Bind] ProductInfo objProd)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                ProductInfo prod = product.GetProductByID(id);
+                product.DeleteProduct(prod);
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProductInfo prod = product.GetProductByID(id);
+
+            return View(prod);
+        }
+    }
+}
---
+++ MVCCrudDemo/Models/ProductDAL.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCrudDemo.Models
+{
+    public class ProductDAL
+    {
+        static List<ProductInfo> productsList = new List<ProductInfo>() {
+                new ProductInfo(){ ID = 0, Name="Xiaomi Essentials", Price="350$"},
+                new ProductInfo(){ ID = 1, Name="Xiaomi m365", Price="400$"},
+                new ProductInfo(){ ID = 2, Name="Xiaomi MI 1 s", Price="450$"},
+                new ProductInfo(){ ID = 3, Name="Xiaomi 3 BLK", Price="550$"},
+                new ProductInfo(){ ID = 4, Name="Xiaomi 2 PRO", Price="600$"}
+            };
+        public IEnumerable<ProductInfo> GetAllProducts()
+        {
+            return productsList;
+        }
+
+        public IEnumerable<ProductInfo> AddProduct(ProductInfo product)
+        {
+            productsList.Add(product);
+
+            return productsList;
+        }
+
+        public IEnumerable<ProductInfo> DeleteProduct(ProductInfo item)
+        {
+            productsList.Remove(item);
+
+            return productsList;
+        }
+
+        public ProductInfo GetProductByID(int? ID)
+        {
+            int index = 0;
+
+            if (ID.HasValue) {
+                index = ID.Value;
+            }
+
+            return productsList[index];
+        }
+
+        public ProductInfo UpdateProduct(ProductInfo objProd) { 
+            return productsList[0];
+        }
+    }
+}
